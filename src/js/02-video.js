057751cab1e54a02867cf8c0263b61ad14096628
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-04-05 12:22:13 +0300

    ,

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+const iframeEl=document.querySelector("#vimeo-player")
+import Player from '@vimeo/player';
+// Створити об'єкт плеєра:
+const player = Player('iframe');
+
+// Отримати збережену позицію відео з локального сховища
+const savedTime = localStorage.getItem('savedTime');
+
+// Якщо збережена позиція відео існує, то встановити її
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
+// Підписатись на подію "ended" для збереження позиції відео при завершенні відтворення
+player.on('ended', function() {
+  localStorage.removeItem('savedTime');
+});
+
+// Підписатись на подію "timeupdate" для збереження позиції відео при зміні часу відео
+player.on('timeupdate', function(data) {
+  localStorage.setItem('savedTime', data.seconds);
+});
 
