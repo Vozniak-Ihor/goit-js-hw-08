3dc4f031b413b6e2fec3b3cabef011c5c2c93699
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-04-05 22:21:05 +0300

    Tack_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,41 @@
-const iframeEl=document.querySelector("#vimeo-player")
+// import throttle from 'lodash.throttle';
+
+// const VIDEO_PLAYER_KEY = "videoplayer-current-time";
+// const videoPlayer = document.querySelector("#video-player");
+// let currentTime = localStorage.getItem(VIDEO_PLAYER_KEY) || 0;
+
+// function saveCurrentTime() {
+//   localStorage.setItem(VIDEO_PLAYER_KEY, videoPlayer.currentTime);
+// }
+
+// videoPlayer.addEventListener("timeupdate", throttle(saveCurrentTime, 1000));
+
+// videoPlayer.currentTime = currentTime;
+
+const iframeEl = document.querySelector('#vimeo-player');
 import Player from '@vimeo/player';
-// Створити об'єкт плеєра:
-const player = Player('iframe');
+let throttle = require('lodash.throttle');
+const player = new Player(iframeEl, {
+  id: 19231868,
+  width: 640,
+});
 
-// Отримати збережену позицію відео з локального сховища
-const savedTime = localStorage.getItem('savedTime');
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    let timeOnPlayer = data.seconds;
+    if (localStorage) {
+      localStorage.setItem('track video playback', timeOnPlayer);
+    } else {
+      console.log(
+        'This browser does not track video playback, if you restart the video will start from the beginning.'
+      );
+    }
+  }, 1000)
+);
 
-// Якщо збережена позиція відео існує, то встановити її
-if (savedTime) {
-  player.setCurrentTime(savedTime);
+if (localStorage.getItem('track video playback')) {
+  player.setCurrentTime(
+    JSON.parse(localStorage.getItem('track video playback'))
+  );
 }
-
-// Підписатись на подію "ended" для збереження позиції відео при завершенні відтворення
-player.on('ended', function() {
-  localStorage.removeItem('savedTime');
-});
-
-// Підписатись на подію "timeupdate" для збереження позиції відео при зміні часу відео
-player.on('timeupdate', function(data) {
-  localStorage.setItem('savedTime', data.seconds);
-});
-
