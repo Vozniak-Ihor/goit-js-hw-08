67d086365e233fe232af0e7b86b13ab1552867dc
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-04-05 13:55:06 +0300

    setup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,31 +2,41 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Change code below this lin
 
-const galleryEl = document.querySelector(".gallery")
-const galleryItemsToString = galleryItems.reduce((acc, {preview, original, description}) => {
-    return acc+=`<li class="gallery__item">
-   <a class="gallery__link" href="${original}">
-      <img loading="lazy" class="gallery__image" 
-            src="${preview}" 
-            alt="${description}" 
-        />
-   </a>
-</li>`
-}, "")
-
-galleryEl.insertAdjacentHTML("beforeend", galleryItemsToString)
-
-console.dir(SimpleLightbox)
-let gallery = new SimpleLightbox('.gallery a', {captionsData: "alt",  captionPosition:"bottom", captionDelay: 250,  scrollZoom: false});
-
-gallery.on('shown.simplelightbox', function (event) {
-  event.preventDefault();
-  console.log("Everything works fine")
-});
-
-gallery.on('error.simplelightbox', function (event) {
-	console.log(event, "Something wrong"); 
-});
 console.log(galleryItems);
+
+function createPhotoCards(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `;
+    })
+    .join("");
+}
+
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createPhotoCards(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  doubleTapZoom: 1.7,
+  alertErrorMessage: "Image not found, next image will be loaded",
+});
+
+gallery.on("shown.simplelightbox", function handleGalleryShown(event) {
+  event.preventDefault();
+  console.log("Everything works fine");
+});
+
+gallery.on("error.simplelightbox", function handleGalleryError(event) {
+  console.log(event, "Something wrong");
+});
