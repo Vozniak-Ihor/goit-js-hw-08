4cf3a4f17eabe558bdbe63127c83f88b84e17e2a
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-04-07 00:00:22 +0300

    Tack_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const { email, message } = formEl.elements;
+const STORAGE_KEY = 'feedback-form-state';
+
+formEl.addEventListener(
+  'input',
+  throttle(() => {
+    localStorage.setItem(
+      STORAGE_KEY,
+      JSON.stringify({ email: email.value, message: message.value })
+    );
+  }),
+  500
+);
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log({email: email.value, message: message.value});
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+});
+
+const populateTextarea = () => {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+    const { email: savedEm = '', message: savedMes = '' } =
+      JSON.parse(savedMessage);
+    email.value = savedEm;
+    message.value = savedMes;
+  }
+};
+
+populateTextarea();
